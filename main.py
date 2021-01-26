f6e8626e351f387252e222a1a22302be18bf1ab0
nhoermann <58261267+nhoermann@users.noreply.github.com>
2021-01-26 16:29:47 +0100

    Function to plot random integers between 100 and 1 in a histogram.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataset = np.random.randint(1,101, 20)
+dataset = np.random.randint(1, 101, 20)
 plt.hist(dataset)
 plt.show()
+
+plt.savefig('rand_data_hist.pdf')
